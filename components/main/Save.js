7a8ebf2f88b28d2de7fdac5eababacf4f60e5c5a
Anish Gurjar <anishgurjar@Anishs-MacBook-Pro.local>
2022-05-20 05:37:06 -0400

    Added image to props and printed path

---
+++ components/main/Save.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, TextInput, Image } from 'react-native'
+
+export default function Save(props){
+    console.log(props.route.params.image)
+    return(
+        <View>
+        </View>
+    );
+}
