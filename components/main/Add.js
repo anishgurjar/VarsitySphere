a4a228bdb83faccdc10240971210c26b6aec641a
Anish Gurjar <anishgurjar@Anishs-MacBook-Pro.local>
2022-05-20 02:54:31 -0400

    IImage Visible in Image Tag

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
   const [type, setType] = useState(CameraType.back);
 
   useEffect(() => {
@@ -17,7 +18,7 @@ export default function App() {
   const takePicture = async () => {
     if(camera){
         const data = await camera.takePictureAsync(null);
-        console.log(data.uri)
+        setImage(data.uri);
     }
   }
 
@@ -46,7 +47,7 @@ export default function App() {
     </Button>
 
     <Button title="Take Picture" onPress={() => takePicture()}/>
-
+    { image && <Image source={{uri: image}} style={{flex: 1}} />}
 
   </View>
   
